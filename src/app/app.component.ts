0d749370582562c062c8684b2898ec081f07bd4c
secreteasy <secreteasy@gmail.com>
2024-06-04 14:52:51 +0300

    frontend

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,18 @@
 import { Component } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { MatIconModule } from '@angular/material/icon';
+import { MatMenuModule } from '@angular/material/menu';
 import { RouterOutlet } from '@angular/router';
 
+
+
 @Component({
-  selector: 'app-root',
+  selector: 'app-root, app-header',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, MatGridListModule, MatButtonModule, MatMenuModule, MatIconModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'project-angular';
 }
